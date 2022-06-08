82dc803d99ac9fca36e38f67f8557eb4f94007fd
Kyle Dominic Lomigo <91366618+KyleDom@users.noreply.github.com>
2022-06-08 12:40:14 +0800

    Add files via upload

---
+++ Scripts/GameManager.cs
@@ -0,0 +1,101 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.Events;
+
+public class GameManager : MonoBehaviour
+{
+    public int health;
+    public int money;
+    private bool gameActive;
+
+    [Header("Components")]
+    public TextMeshProUGUI healthAndMoneyText;
+    public EnemyPath enemyPath;
+    public TowerPlacement towerPlacement;
+    public EndScreenUI endScreen;
+    public WaveSpawner waveSpawner;
+
+    [Header("Events")]
+    public UnityEvent onMoneyChanged;
+
+    // Singleton
+    public static GameManager instance;
+
+
+    void OnDisable ()
+    {
+        Enemy.OnDestroyed -= OnEnemyDestroyed;
+    }
+
+    void Awake ()
+    {
+        instance = this;
+    }
+
+    void Start ()
+    {
+        gameActive = true;
+        UpdateHealthAndMoneyText();
+    }
+
+    void UpdateHealthAndMoneyText ()
+    {
+        healthAndMoneyText.text = $"Health: {health}\nCredits: {money}";
+    }
+
+    public void AddMoney (int amount)
+    {
+        money += amount;
+        UpdateHealthAndMoneyText();
+
+        onMoneyChanged.Invoke();
+    }
+
+    public void TakeMoney (int amount)
+    {
+        money -= amount;
+        UpdateHealthAndMoneyText();
+
+        onMoneyChanged.Invoke();
+    }
+
+    public void TakeDamage (int amount)
+    {
+        health -= amount;
+        UpdateHealthAndMoneyText();
+
+        if(health <= 0)
+            GameOver();
+    }
+
+    void GameOver ()
+    {
+        gameActive = false;
+        endScreen.gameObject.SetActive(true);
+        endScreen.SetEndScreen(false, waveSpawner.curWave);
+    }
+
+    void WinGame ()
+    {
+        gameActive = false;
+        endScreen.gameObject.SetActive(true);
+        endScreen.SetEndScreen(true, waveSpawner.curWave);
+    }
+
+    public void OnEnemyDestroyed ()
+    {
+        if(!gameActive)
+            return;
+
+        if(waveSpawner.remainingEnemies == 0 && waveSpawner.curWave == waveSpawner.waves.Length)
+        {
+            WinGame();
+        }
+    }
+}
+
+   
+   
+
---
+++ Scripts/Towers/Tower.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Tower : MonoBehaviour 
+{
+     public enum TargetPriority
+    {
+        First,
+        Close,
+        Strong
+    }
+
+     [Header("Info")]
+    public float range;
+    protected List<Enemy> currentEnemiesInRange = new List<Enemy>();
+    protected Enemy currentEnemy;
+    public TargetPriority targetPriority;
+    public bool rotateTowardsTarget;
+
+    [Header("Attacking")]
+    public float attackRate;
+    protected float lastAttackTime;
+    public GameObject projectilePrefab;
+    public Transform projectileSpawnPos;
+
+    public int projectileDamage;
+    public float projectileSpeed;
+
+    protected abstract void Update();
+    protected abstract Enemy GetEnemy();
+    protected abstract void Attack();
+    protected abstract void OnTriggerEnter(Collider other);
+    protected abstract void OnTriggerExit(Collider other);
+} 
+
